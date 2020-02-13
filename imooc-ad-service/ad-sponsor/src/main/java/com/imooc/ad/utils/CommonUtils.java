0b33f1d78273e88c9c349f3c96b97971a5675830
zwyu123 <zwyu2017@163.com>
2020-02-13 13:52:56 +0800

    推广计划服务

--- imooc-ad-service/ad-sponsor/src/main/java/com/imooc/ad/utils/CommonUtils.java
+++ imooc-ad-service/ad-sponsor/src/main/java/com/imooc/ad/utils/CommonUtils.java
@@ -8,8 +8,24 @@ import java.util.Date;
 
 public class CommonUtils {
 
+    private static String[] parsePatterns = {
+            "yyyy-MM-dd", "yyyy/MM/dd", "yyyy.MM.dd"
+    };
+
     public static String md5(String value) {
 
         return DigestUtils.md5Hex(value).toUpperCase();
+    }
+
+    public static Date parseStringDate(String dateString)
+            throws AdException {
+
+        try {
+            return DateUtils.parseDate(
+                    dateString, parsePatterns
+            );
+        } catch (Exception ex) {
+            throw new AdException(ex.getMessage());
+        }
     }
 }

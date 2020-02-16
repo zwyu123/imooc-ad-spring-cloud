2e031f6633d105505a2e4cf45282486bb5759b25
zwyu123 <zwyu2017@163.com>
2020-02-16 17:00:23 +0800

    创意与推广单元关联索引对象定义与服务实现

---
+++ imooc-ad-service/ad-search/src/main/java/com/imooc/ad/index/creativeunit/CreativeUnitObject.java
@@ -0,0 +1,14 @@
+package com.imooc.ad.index.creativeunit;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreativeUnitObject {
+
+    private Long adId;
+    private Long unitId;
+}

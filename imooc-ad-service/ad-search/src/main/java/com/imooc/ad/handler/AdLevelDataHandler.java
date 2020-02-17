61885bdd152b9ff6a84d930b71137dbcfd730659
zwyu123 <zwyu2017@163.com>
2020-02-17 13:51:28 +0800

    第四层级索引操作实现

--- imooc-ad-service/ad-search/src/main/java/com/imooc/ad/handler/AdLevelDataHandler.java
+++ imooc-ad-service/ad-search/src/main/java/com/imooc/ad/handler/AdLevelDataHandler.java
@@ -1,10 +1,7 @@
 package com.imooc.ad.handler;
 
 import com.alibaba.fastjson.JSON;
-import com.imooc.ad.dump.table.AdCreativeTable;
-import com.imooc.ad.dump.table.AdCreativeUnitTable;
-import com.imooc.ad.dump.table.AdPlanTable;
-import com.imooc.ad.dump.table.AdUnitTable;
+import com.imooc.ad.dump.table.*;
 import com.imooc.ad.index.DataTable;
 import com.imooc.ad.index.IndexAware;
 import com.imooc.ad.index.adplan.AdPlanIndex;
@@ -15,9 +12,16 @@ import com.imooc.ad.index.creative.CreativeIndex;
 import com.imooc.ad.index.creative.CreativeObject;
 import com.imooc.ad.index.creativeunit.CreativeUnitIndex;
 import com.imooc.ad.index.creativeunit.CreativeUnitObject;
+import com.imooc.ad.index.district.UnitDistrictIndex;
+import com.imooc.ad.index.interest.UnitItIndex;
+import com.imooc.ad.index.keyword.UnitKeywordIndex;
 import com.imooc.ad.mysql.constant.OpType;
 import com.imooc.ad.utils.CommonUtils;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * 1. 索引之间存在着层级的划分，也就是依赖关系的划分
@@ -122,6 +126,92 @@ public class AdLevelDataHandler {
                         creativeUnitObject.getUnitId().toString()
                 ),
                 creativeUnitObject,
+                type
+        );
+    }
+
+    public static void handleLevel4(AdUnitDistrictTable unitDistrictTable,
+                                    OpType type) {
+
+        if (type == OpType.UPDATE) {
+            log.error("district index can not support update");
+            return;
+        }
+
+        AdUnitObject unitObject = DataTable.of(
+                AdUnitIndex.class
+        ).get(unitDistrictTable.getUnitId());
+        if (unitObject == null) {
+            log.error("AdUnitDistrictTable index error: {}",
+                    unitDistrictTable.getUnitId());
+            return;
+        }
+
+        String key = CommonUtils.stringConcat(
+                unitDistrictTable.getProvince(),
+                unitDistrictTable.getCity()
+        );
+        Set<Long> value = new HashSet<>(
+                Collections.singleton(unitDistrictTable.getUnitId())
+        );
+        handleBinlogEvent(
+                DataTable.of(UnitDistrictIndex.class),
+                key, value,
+                type
+        );
+    }
+
+    public static void handleLevel4(AdUnitItTable unitItTable, OpType type) {
+
+        if (type == OpType.UPDATE) {
+            log.error("it index can not support update");
+            return;
+        }
+
+        AdUnitObject unitObject = DataTable.of(
+                AdUnitIndex.class
+        ).get(unitItTable.getUnitId());
+        if (unitObject == null) {
+            log.error("AdUnitItTable index error: {}",
+                    unitItTable.getUnitId());
+            return;
+        }
+
+        Set<Long> value = new HashSet<>(
+                Collections.singleton(unitItTable.getUnitId())
+        );
+        handleBinlogEvent(
+                DataTable.of(UnitItIndex.class),
+                unitItTable.getItTag(),
+                value,
+                type
+        );
+    }
+
+    public static void handleLevel4(AdUnitKeywordTable keywordTable,
+                                    OpType type) {
+
+        if (type == OpType.UPDATE) {
+            log.error("keyword index can not support update");
+            return;
+        }
+
+        AdUnitObject unitObject = DataTable.of(
+                AdUnitIndex.class
+        ).get(keywordTable.getUnitId());
+        if (unitObject == null) {
+            log.error("AdUnitKeywordTable index error: {}",
+                    keywordTable.getUnitId());
+            return;
+        }
+
+        Set<Long> value = new HashSet<>(
+                Collections.singleton(keywordTable.getUnitId())
+        );
+        handleBinlogEvent(
+                DataTable.of(UnitKeywordIndex.class),
+                keywordTable.getKeyword(),
+                value,
                 type
         );
     }

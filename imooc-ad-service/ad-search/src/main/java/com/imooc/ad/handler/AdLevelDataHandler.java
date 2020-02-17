d9d574cd8b0b683e1117b7286c3e3a3320f77386
zwyu123 <zwyu2017@163.com>
2020-02-17 11:08:49 +0800

    第三层级索引操作实现

--- imooc-ad-service/ad-search/src/main/java/com/imooc/ad/handler/AdLevelDataHandler.java
+++ imooc-ad-service/ad-search/src/main/java/com/imooc/ad/handler/AdLevelDataHandler.java
@@ -1,14 +1,22 @@
 package com.imooc.ad.handler;
 
+import com.alibaba.fastjson.JSON;
 import com.imooc.ad.dump.table.AdCreativeTable;
+import com.imooc.ad.dump.table.AdCreativeUnitTable;
 import com.imooc.ad.dump.table.AdPlanTable;
+import com.imooc.ad.dump.table.AdUnitTable;
 import com.imooc.ad.index.DataTable;
 import com.imooc.ad.index.IndexAware;
 import com.imooc.ad.index.adplan.AdPlanIndex;
 import com.imooc.ad.index.adplan.AdPlanObject;
+import com.imooc.ad.index.adunit.AdUnitIndex;
+import com.imooc.ad.index.adunit.AdUnitObject;
 import com.imooc.ad.index.creative.CreativeIndex;
 import com.imooc.ad.index.creative.CreativeObject;
+import com.imooc.ad.index.creativeunit.CreativeUnitIndex;
+import com.imooc.ad.index.creativeunit.CreativeUnitObject;
 import com.imooc.ad.mysql.constant.OpType;
+import com.imooc.ad.utils.CommonUtils;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -51,6 +59,69 @@ public class AdLevelDataHandler {
                 DataTable.of(CreativeIndex.class),
                 creativeObject.getAdId(),
                 creativeObject,
+                type
+        );
+    }
+
+    public static void handleLevel3(AdUnitTable unitTable, OpType type) {
+
+        AdPlanObject adPlanObject = DataTable.of(
+                AdPlanIndex.class
+        ).get(unitTable.getPlanId());
+        if (null == adPlanObject) {
+            log.error("handleLevel3 found AdPlanObject error: {}",
+                    unitTable.getPlanId());
+            return;
+        }
+
+        AdUnitObject unitObject = new AdUnitObject(
+                unitTable.getUnitId(),
+                unitTable.getUnitStatus(),
+                unitTable.getPositionType(),
+                unitTable.getPlanId(),
+                adPlanObject
+        );
+
+        handleBinlogEvent(
+                DataTable.of(AdUnitIndex.class),
+                unitTable.getUnitId(),
+                unitObject,
+                type
+        );
+    }
+
+    public static void handleLevel3(AdCreativeUnitTable creativeUnitTable,
+                                    OpType type) {
+
+        if (type == OpType.UPDATE) {
+            log.error("CreativeUnitIndex not support update");
+            return;
+        }
+
+        AdUnitObject unitObject = DataTable.of(
+                AdUnitIndex.class
+        ).get(creativeUnitTable.getUnitId());
+        CreativeObject creativeObject = DataTable.of(
+                CreativeIndex.class
+        ).get(creativeUnitTable.getAdId());
+
+        if (null == unitObject || null == creativeObject) {
+            log.error("AdCreativeUnitTable index error: {}",
+                    JSON.toJSONString(creativeUnitTable));
+            return;
+        }
+
+        CreativeUnitObject creativeUnitObject = new CreativeUnitObject(
+                creativeUnitTable.getAdId(),
+                creativeUnitTable.getUnitId()
+        );
+        handleBinlogEvent(
+                DataTable.of(CreativeUnitIndex.class),
+                CommonUtils.stringConcat(
+                        creativeUnitObject.getAdId().toString(),
+                        creativeUnitObject.getUnitId().toString()
+                ),
+                creativeUnitObject,
                 type
         );
     }
